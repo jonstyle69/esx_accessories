b76e728a44f66907251f53fa9ed44572f1369a08
jonstyle69 <44425507+jonstyle69@users.noreply.github.com>
2018-11-01 16:32:51 +0800

    add tw.lua

---
+++ locales/tw.lua
@@ -0,0 +1,21 @@
+Locales ['tw'] = {
+
+	['valid_purchase'] = '確定要購買？',
+	['yes'] = '是',
+	['no'] = '否',
+	['helmet'] = '頭盔/帽子',
+	['glasses'] = '眼鏡',
+	['mask'] = '面具',
+	['ears'] = '耳環',
+	['shop'] = '商店',
+	['set_unset'] = '戴上/脫下',
+	['not_enough_money'] = '你的錢不夠',
+	['press_access'] = '按~INPUT_CONTEXT~進入選單',
+	['accessories_blip'] = '配件',
+	['no_ears'] = '你沒有耳環',
+	['no_glasses'] = '你沒有眼鏡',
+	['no_helmet'] = '你沒有頭盔',
+	['no_mask'] = '你沒有面具',
+	['you_paid'] = '你已付款',
+
+}
